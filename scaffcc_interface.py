65ed87c10b7e378cc5ac5319c787c87abbb490dd
Pranav Gokhale <pranavgokhale@uchicago.edu>
2018-06-30 11:42:57 -0700

    Remove kwarg for disabling rotation decomposition
    disabled by default in new release of ScaffCC

--- scaffcc_interface.py
+++ scaffcc_interface.py
@@ -21,22 +21,15 @@ class ScaffCC():
     scaffold_filename = 'tmp.scaffold'
     openqasm_filename = 'tmp.qasm'
 
-    def __init__(self, scaffold_code, scaffold_path='../ScaffCC/scaffold.sh',
-                 disable_rotation_decomposition=False):
+    def __init__(self, scaffold_code, scaffold_path='../ScaffCC/scaffold.sh'):
         self.scaffold_code = scaffold_code
         self.scaffold_path = scaffold_path
-        self.disable_rotation_decomposition = disable_rotation_decomposition
         assert subprocess.run(['test', '-f', self.scaffold_path]).returncode == 0, \
-            'Cannot find scaffold.sh at the given path: %s' % self.scaffold_path
+            'Could not run ./%s (did you provide path correctly?)' % self.scaffcc_path
 
     def get_openqasm(self):
         self._write_scaffold_file()
-
-        compiler_flag = '-b'
-        if self.disable_rotation_decomposition:
-            compiler_flag += 'R'
-        subprocess.run(['bash', self.scaffold_path, compiler_flag, self.scaffold_filename])
-
+        subprocess.run(['bash', self.scaffold_path, '-b', self.scaffold_filename])
         openqasm = self._get_openqasm()
         self._cleanup()
         return openqasm
